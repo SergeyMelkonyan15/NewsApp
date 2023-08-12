34f55670796a363fb285c00a491e97092782394f
Sergey Melkonyan <sergey.melkonyanwork@gmail.com>
2023-08-12 16:37:35 +0400

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Sergey Melkonyan on 12.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
